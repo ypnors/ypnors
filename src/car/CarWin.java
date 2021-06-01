11792b56a8b1275cb1c65ca947b853fc0ce2b11e
ypnors <godhype@gmail.com>
2021-06-01 15:37:06 +0800

    Initial commit

---
+++ src/car/CarWin.java
@@ -0,0 +1,12 @@
+package car;
+public class CarWin {
+    static final int Width=600;
+
+    public static void main(String[] args) {
+        JFrame frame=new Jframe();
+        frame.setSize(Width,50);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisiable(true);
+
+    }
+}
---
+++ src/car/Tester.java
@@ -0,0 +1,55 @@
+package car;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Tester {
+    public static void main(String[] args) {
+        int fee = 30;
+        Map<String, Car> cars = new HashMap<>();
+        Car c1 = new Car("abc-123", "07:30");
+        Car c2 = new Car("xyz-123", "09:45");
+        Car c3 = new Car("ghi-123", "10:30");
+        cars.put(c1.id, c1);
+        cars.put(c2.id, c2);
+        cars.put(c3.id, c3);
+        String id = "abc-123";
+        Car car = cars.get(id);
+        if (car == null) {
+            System.out.println("Car not found");
+        } else {
+            Date now = new Date();
+            long ms = now.getTime() - car.enter.getTime();
+            long mins = ms / (1000 * 60);
+            System.out.println(mins);
+        }
+
+
+//        practice();
+    }
+
+    private static void practice() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        try {
+            Date d = sdf.parse("08:58");
+            System.out.println(d);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 8);
+        cal.set(Calendar.MINUTE, 30);
+
+        System.out.println(cal.getTime());
+
+        Car c1 = new Car("abc-123");
+        Car c2 = new Car("xyz-888", "07:30");
+        System.out.println(c1.enter);
+        Date now = new Date();
+        System.out.println(now.getTime());
+        long dis = now.getTime() - c1.enter.getTime();
+        System.out.println(dis);
+    }
+}
